2c3c1669223acdd5c848df9cbf5eb54df000ddc7
Kelly Redding <kelly@kellyredding.com>
2013-10-15 23:32:38 -0500

    have `RunError` *optionally* take a custom backtrace
    This updates the RunError custom exception to be optionally created
    with a custom backtrace.  If none is provided, it falls back to the
    `caller`.  This makes it behave like any other exception class.

--- lib/scmd.rb
+++ lib/scmd.rb
@@ -7,4 +7,13 @@ module Scmd
     Command.new(*args, &block)
   end
 
+  TimeoutError = Class.new(::RuntimeError)
+
+  class RunError < ::RuntimeError
+    def initialize(stderr, called_from = nil)
+      super(stderr)
+      set_backtrace(called_from || caller)
+    end
+  end
+
 end
